a001cef219e38825e8a251a03622e3722edd092e
marit.van.dijk <mlvandijk@gmail.com>
2022-12-10 18:46:08 +0100

    Ask for user's name

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello, what is your name?");
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name + "!");
     }
 }
