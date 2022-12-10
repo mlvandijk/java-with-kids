3297bb6f5b728fd5a8ea2dd0ceab9739647fbfb2
marit.van.dijk <mlvandijk@gmail.com>
2022-12-10 19:05:02 +0100

    Make sure to get valid input

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,6 +10,17 @@ public class Main {
         System.out.println("Typ NE voor Nederlands. \nType EN for English.");
         String language = scanner.nextLine();
 
+        boolean validLanguage = false;
+
+        while (!validLanguage) {
+            if ((Objects.equals(language, "NE") || Objects.equals(language, "EN"))) {
+                validLanguage = true;
+            } else {
+                System.out.println("Sorry, dat begrijp ik niet. Typ NE voor Nederlands. \nSorry, I don't understand. Type EN for English.");
+                language = scanner.nextLine();
+            }
+        }
+
         if (Objects.equals(language, "NE")) {
             System.out.println("Hallo, hoe heet jij?");
             String name = scanner.nextLine();
@@ -26,6 +37,6 @@ public class Main {
             String color = scanner.nextLine();
 
             System.out.println(color + " is a pretty color. My favorite color is pink.");
-        } // "But what if they type something other than "NE" or "NL"?"
+        }
     }
 }
