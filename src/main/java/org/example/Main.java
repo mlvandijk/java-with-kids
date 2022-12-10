e45dd69ca76f5b081c6682e298003da6515e3f49
marit.van.dijk <mlvandijk@gmail.com>
2022-12-10 18:50:44 +0100

    Ask for name and favorite color

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,10 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hello, what is your name?");
         String name = scanner.nextLine();
-        System.out.println("Hello " + name + "!");
+
+        System.out.println("Hello " + name + ", what is your favorite color?");
+        String color = scanner.nextLine();
+
+        System.out.println(color + " is a pretty color. My favorite color is pink.");
     }
 }
